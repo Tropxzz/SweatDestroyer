f548f63a442b267c522e13b945facd0a3a3917ca
Tropxzz <130433432+Tropxzz@users.noreply.github.com>
2023-06-03 23:06:41 +1000

    Create MainOperator.lua

---
+++ MainOperator.lua
@@ -0,0 +1,11 @@
+if game.PlaceId == 3956818381 then -- ninja legends
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/SweatDestroyer/main/NinjaLegends.lua", true))()
+   elseif game.PlaceId == 2580982329 then -- texting sim
+       loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/SweatDestroyer/main/PhoneTextingsimulator.lua", true))()
+elseif game.PlaceId ==155615604 then -- Prison life
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/SweatDestroyer/main/prisonlife.lua", true))()
+    elseif game.PlaceId ==1962086868 then -- Toh
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/SweatDestroyer/main/Toh.lua", true))()
+    elseif game.PlaceId ~= 155615604 or 2580982329 or 3956818381 or 1962086868 then -- Universal
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/SweatDestroyer/main/Universal", true))()
+end
