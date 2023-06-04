9808c0814a6d78782dd305b4cb6dbac24906dcaa
Tropxzz <130433432+Tropxzz@users.noreply.github.com>
2023-06-04 17:43:33 +1000

    Update BABFT.lua

--- BABFT.lua
+++ BABFT.lua
@@ -5,19 +5,6 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/1201for/V.G_Hub_Extra
    print("but fr why u looking")
    print("Here me out, im not a skid totally ")
 
-   local Foldername = "Negar"
-   local overrideDatabase = true
-   
-   if (isfolder(Foldername) and overrideDatabase) then
-   elseif (isfolder(Foldername) and not overrideDatabase) then
-       return warn("Overriding the database is automatically disabled for security purposes.")
-   else
-       makefolder(Foldername)
-   end
-   
-   writefile(Foldername.."\\GangsterGangster.txt", "If you reading this ur a gangster :) Made by Tropxz#5665.")
-   writefile(Foldername.."\\MainScript.lua", "Not made yet")
-
 
 local Players = game:GetService("Players")
 local rs = game:GetService("ReplicatedStorage")
