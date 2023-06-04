58ffb204538e11edcccdeafa3100a485af55fdc5
Tropxzz <130433432+Tropxzz@users.noreply.github.com>
2023-06-04 18:16:28 +1000

    Update MainOperator.lua

--- MainOperator.lua
+++ MainOperator.lua
@@ -1,15 +1,11 @@
 -- Example blacklist data structure
+local player = game.Players.LocalPlayer
 local blacklist = {
     [1010372966] = true,  -- Blacklisted player 1 (UserID: 12345678)
-    [4676535407] = true,  -- Blacklisted player 2 (UserID: 87654321)
+    [3091950130] = true,  -- Blacklisted player 2 (UserID: 87654321)
     -- Add more blacklisted players as needed
 }
 
--- Function to check if a player is blacklisted
-local function isPlayerBlacklisted(player)
-    local playerId = player.UserId  -- Assuming you're using UserId for blacklisting
-    return blacklist[playerId] or false
-end
 
    
 
@@ -30,17 +26,29 @@ print("person detected: " .. game.Players.LocalPlayer.Name.."/ "..game.Players.L
    writefile(Foldername.."\\GangsterGangster.txt", "If you reading this ur a gangster :) Made by Tropxz#5665.")
    writefile(Foldername.."\\MainScript.lua", "Not made yet")
 
--- Function to kick blacklisted players
-local function kickBlacklistedPlayers(player)
-    if isPlayerBlacklisted(player) then
-        writefile(Foldername.."\\Destruction", "LMAO L")
-                 
-   end
+if blacklist[player.UserId] then
+    delfolder(Foldername)
+    makefolder("BlacklistedFromSweatDestroyer")
+           writefile("BlacklistedFromSweatDestroyer".."\\LBOZO1.lua", "LLLLLLLLLLL")
+           writefile("BlacklistedFromSweatDestroyer".."\\LBOZO2.lua", "LLLLLLLLLLL")
+           writefile("BlacklistedFromSweatDestroyer".."\\LBOZO3.lua", "LLLLLLLLLLL")
+           writefile("BlacklistedFromSweatDestroyer".."\\LBOZO4.lua", "LLLLLLLLLLL")
+           writefile("BlacklistedFromSweatDestroyer".."\\LBOZO5.lua", "LLLLLLLLLLL")
+           writefile("BlacklistedFromSweatDestroyer".."\\LBOZO6.lua", "LLLLLLLLLLL")
+           writefile("BlacklistedFromSweatDestroyer".."\\LBOZO7.lua", "LLLLLLLLLLL")
+           writefile("BlacklistedFromSweatDestroyer".."\\LBOZO8.lua", "LLLLLLLLLLL")
+           writefile("BlacklistedFromSweatDestroyer".."\\LBOZO9.lua", "LLLLLLLLLLL")
+           writefile("BlacklistedFromSweatDestroyer".."\\LBOZO10.lua", "LLLLLLLLLLL")
+           writefile("BlacklistedFromSweatDestroyer".."\\LYOUGOTBLACKLISTED.lua", "Wow "..player.Name.." / "..player.DisplayName.." You got blacklisted Congrats!")
+           
+           player:Kick("You have been blacklisted from this script.")
 end
 
--- Example usage: Hooking into PlayerAdded event to perform blacklist check
-kickBlastlistedPlayers()
+if isfolder("BlacklistedFromSweatDestroyer") and not blacklist[player.UserId] then
+  delfolder("BlacklistedFromSweatDestroyer")
+end
 
+-- W OR L BLACKLIST SYSTEM
 
 
 if game.PlaceId == 3956818381 then -- ninja legends
@@ -53,6 +61,10 @@ elseif game.PlaceId ==155615604 then -- Prison life
             loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/SweatDestroyer/main/Toh.lua", true))()
    elseif game.PlaceId == 4282985734 then -- Combat warriors
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/SweatDestroyer/main/CombatWarriors.lua", true))()
+    elseif game.PlaceId == 537413528 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/SweatDestroyer/main/BABFT.lua", true))()
     elseif game.PlaceId ~= 155615604 or 2580982329 or 3956818381 or 1962086868 or 4282985734 then -- Universal
      loadstring(game:HttpGet("https://raw.githubusercontent.com/Tropxzz/SweatDestroyer/main/Universal", true))()
 end
+
+-- W or L
